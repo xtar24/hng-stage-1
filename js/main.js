dc64576462c161e13bf09fe7198a77ae8931f3d1
xtar24 <akinfetolulope@gmail.com>
2024-07-06 19:39:35 +0100

    initial commt

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
         document.getElementById('currentDay').textContent=now.toLocaleString('en-US',{weekday:'long'});
     }
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburgerButton = document.querySelector (".hamburger-button");
+    const mobileMenu = document.querySelector(".mobile-menu");
+
+    hamburgerButton.addEventListener('click', ()=> {
+        mobileMenu.classList.toggle("active")
+    });
+});
