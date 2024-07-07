468b087c5ff995b0b388cc37a90c704967b24e82
xtar24 <akinfetolulope@gmail.com>
2024-07-07 19:59:25 +0100

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,35 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    const updateTime = () => {
-        const now =new Date();
-        document.getElementById('currentTime').textContent=now.toUTCString();
-        document.getElementById('currentDay').textContent=now.toLocaleString('en-US',{weekday:'long'});
-    }
-})
+function updateDateTime() {
+    const now = new Date();
+    const dayOfWeek = now.toLocaleDateString('en-US', { weekday: 'long' });
+    const time = now.toLocaleTimeString('en-US', { hour12: false });
+    const date = now.toLocaleDateString('en-US');
+
+    document.getElementById('currentDay').textContent = `Current UTC Time: ${time} | Date: ${date} | Day: ${dayOfWeek}`;
+}
+
+// Update the date and time every second
+setInterval(updateDateTime, 1000);
+
+// Initial call to display the date and time immediately when the page loads
+updateDateTime();
+
+
+
+
+const hamburgerButton = document.querySelector('.hamburger-button');
+
+const mainMenu = document.querySelector('.main-menu');
+
+hamburgerButton.addEventListener('click', ()=> {
+    hamburgerButton.classList.toggle('active');
+    mainMenu.classList.toggle('active');
+});
 
 document.addEventListener("DOMContentLoaded", () => {
-    const hamburgerButton = document.querySelector (".hamburger-button");
-    const mobileMenu = document.querySelector(".mobile-menu");
-
-    hamburgerButton.addEventListener('click', ()=> {
-        mobileMenu.classList.toggle("active")
+    const hamburgerButton = document.querySelector(".hamburger-button");
+    const mainMenu = document.querySelector(".main-menu");
+  
+    hamburgerButton.addEventListener('click', () =>{
+        mainMenu.classList.toggle("active")
     });
-});
+  });
